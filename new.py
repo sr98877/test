69ceeb556d9dc3afdbaf6bf307e0f4f3e26072b5
sr98877 <srronak11@gmail.com>
2021-12-09 22:05:41 +0530

    new data added

---
+++ new.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_world():
+    print("it is my world")
+    print("new world of mine")
+    x = 1
+    if x < 1:
+        raise Exception("erro")
+        raise TypeError("error")
+print("new file added:") 
